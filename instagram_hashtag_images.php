bca23b5a351646a33b28c00e176779c9da740191
Shawn Graham <shawn_graham@carleton.ca>
2016-09-22 09:38:17 -0400

    Update instagram_hashtag_images.php

--- instagram_hashtag_images.php
+++ instagram_hashtag_images.php
@@ -4,7 +4,7 @@ function scrape_insta_hash($tag) {
 	$shards = explode('window._sharedData = ', $insta_source);
 	$insta_json = explode(';</script>', $shards[1]);
 	$insta_array = json_decode($insta_json[0], TRUE);
-	//print_r(array_values($insta_array)); // to see what's inside
+	//print_r(array_values($insta_array));
 	return $insta_array; // this return a lot things print it and see what else you need
 }
 $tag = 'humanbone'; // tag for which ou want images
@@ -13,14 +13,18 @@ $limit = 7; // provide the limit thats important because one page only give some
 $image_array = array(); // array to store images.
 	for ($i=0; $i < $limit; $i++) {
 		$latest_array = $results_array['entry_data']['TagPage'][0]['tag']['media']['nodes'][$i];
-	 	$image_data  = ' <img src="'.$latest_array['thumbnail_src'].'"> '; // thumbnail and same sizes
+		$owner_array = $latest_array['owner'];
+		$owner_data = 'OWNER '.$owner_array['id'].' ENDOWNER ';
+		$image_data  = ' <img src="'.$latest_array['thumbnail_src'].'"> '; // thumbnail and same sizes
 		$caption_data = ' CAPTIONS '.$latest_array['caption'].' CAPTIONSEND';
 		$likes_array = $latest_array['likes'];
-		$likes_data = ' COUNT-LIKES '.$likes_array['count'].' LIKESEND '; //18 and 19 gets the count which is inside likes
+		$likes_data = ' COUNT-LIKES '.$likes_array['count'].' LIKESEND ';
 		$comments_array = $latest_array['comments'];
-		$comments_data = ' COUNT-COMMENTS '.$comments_array['count'].' COMMENTSEND '; //this should do the same but isn't.
+		$comments_data = ' COUNT-COMMENTS '.$comments_array['count'].' COMMENTSEND ';
+
+		//'LIKES_AND_COMMENTS'.$latest_array['likes']['comments'].' LC_END';
 		//$image_data  = '<img src="'.$latest_array['display_src'].'">'; actual image and different sizes
-		array_push($image_array, $image_data, $caption_data, $likes_data, $comments_data);
+		array_push($image_array, $owner_data, $image_data, $caption_data, $likes_data, $comments_data);
 	}
 	foreach ($image_array as $image) {
 		echo $image;// this will echo the images wrap it in div or ul li what ever html structure
